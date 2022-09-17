bd5a955aff7a85cbf6eb50e35c4fc9231cfda82b
Mithun Biswas <bhoot.biswas@gmail.com>
2022-09-17 13:38:54 +0600

    Update OrderSummary()

--- amortizable-growth-ext/components/cart/OrderSummary.js
+++ amortizable-growth-ext/components/cart/OrderSummary.js
@@ -5,10 +5,20 @@ import { Text, Money } from 'components';
 export function OrderSummary() {
     const { cart } = useCart();
     const { items_subtotal_price, currency } = cart;
+    const totalSaving = cart.total_discount ? <Money data={{ amount: String(items_subtotal_price), currencyCode: currency }} /> : null;
 
     return (
         <>
             <dl className="OrderSummary">
+                {totalSaving && (
+                    <div className="OrderSummary__item">
+                        <Text as="dt">Rewards</Text>
+                        <Text as="dd">
+                            You are saving {totalSaving}
+                        </Text>
+                    </div>
+                )}
+
                 <div className="OrderSummary__item">
                     <Text as="dt">Subtotal</Text>
                     <Text as="dd">
