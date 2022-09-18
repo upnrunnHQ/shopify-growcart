235744d1294ba3b0480de414142a3ce4bba5b445
Mithun Biswas <bhoot.biswas@gmail.com>
2022-09-18 15:17:33 +0600

    Update growcart-db.js

--- web/growcart-db.js
+++ web/growcart-db.js
@@ -5,7 +5,7 @@ import { Shopify } from "@shopify/shopify-api";
 const DEFAULT_DB_FILE = path.join(process.cwd(), "growcart.sqlite");
 const DEFAULT_PURCHASE_QUANTITY = 1;
 
-export const GrowCartBD = {
+export const GrowCartDB = {
     cartSettingsTableName: "cart_settings",
     db: null,
     ready: null,
@@ -18,11 +18,11 @@ export const GrowCartBD = {
     }) {
         await this.ready;
         const query = `
-        INSERT INTO ${this.cartSettingsTableName}
-        (shopDomain, minimumRequiremenType, discountType, discounts)
-        VALUES (?, ?, ?, ?)
-        RETURNING id;
-        `;
+      INSERT INTO ${this.cartSettingsTableName}
+      (shopDomain, minimumRequiremenType, discountType, discounts)
+      VALUES (?, ?, ?, ?)
+      RETURNING id;
+    `;
 
         const rawResults = await this.__query(query, [
             shopDomain,
@@ -37,13 +37,9 @@ export const GrowCartBD = {
     update: async function (
         id,
         {
-            title,
-            productId,
-            variantId,
-            handle,
-            discountId,
-            discountCode,
-            destination,
+            minimumRequiremenType,
+            discountType,
+            discounts,
         }
     ) {
         await this.ready;
@@ -51,25 +47,17 @@ export const GrowCartBD = {
         const query = `
       UPDATE ${this.cartSettingsTableName}
       SET
-        title = ?,
-        productId = ?,
-        variantId = ?,
-        handle = ?,
-        discountId = ?,
-        discountCode = ?,
-        destination = ?
+        minimumRequiremenType = ?,
+        discountType = ?,
+        discounts = ?
       WHERE
         id = ?;
     `;
 
         await this.__query(query, [
-            title,
-            productId,
-            variantId,
-            handle,
-            discountId,
-            discountCode,
-            destination,
+            minimumRequiremenType,
+            discountType,
+            discounts,
             id,
         ]);
         return true;
@@ -82,9 +70,7 @@ export const GrowCartBD = {
       WHERE shopDomain = ?;
     `;
 
-        const results = await this.__query(query, [shopDomain]);
-
-        return results.map((qrcode) => this.__addImageUrl(qrcode));
+        return await this.__query(query, [shopDomain]);
     },
 
     read: async function (id) {
@@ -96,7 +82,7 @@ export const GrowCartBD = {
         const rows = await this.__query(query, [id]);
         if (!Array.isArray(rows) || rows?.length !== 1) return undefined;
 
-        return this.__addImageUrl(rows[0]);
+        return rows[0];
     },
 
     delete: async function (id) {
@@ -109,41 +95,13 @@ export const GrowCartBD = {
         return true;
     },
 
-    /* The destination URL for a QR code is generated at query time */
-    generateQrcodeDestinationUrl: function (qrcode) {
-        return `${Shopify.Context.HOST_SCHEME}://${Shopify.Context.HOST_NAME}/qrcodes/${qrcode.id}/scan`;
-    },
-
-    /* The behavior when a QR code is scanned */
-    handleCodeScan: async function (qrcode) {
-
-        /* Log the scan in the database */
-        await this.__increaseScanCount(qrcode);
-
-        const url = new URL(qrcode.shopDomain);
-        switch (qrcode.destination) {
-
-            /* The QR code redirects to the product view */
-            case "product":
-                return this.__goToProductView(url, qrcode);
-
-            /* The QR code redirects to checkout */
-            case "checkout":
-                return this.__goToProductCheckout(url, qrcode);
-
-            default:
-                throw `Unrecognized destination "${qrcode.destination}"`;
-        }
-    },
-
     /* Private */
 
     /*
       Used to check whether to create the database.
       Also used to make sure the database and table are set up before the server starts.
     */
-
-    __hasQrCodesTable: async function () {
+    __hasSettingsTable: async function () {
         const query = `
       SELECT name FROM sqlite_schema
       WHERE
@@ -160,25 +118,19 @@ export const GrowCartBD = {
         /* Initializes the connection to the database */
         this.db = this.db ?? new sqlite3.Database(DEFAULT_DB_FILE);
 
-        const hasQrCodesTable = await this.__hasQrCodesTable();
+        const hasSettingsTable = await this.__hasSettingsTable();
 
-        if (hasQrCodesTable) {
+        if (hasSettingsTable) {
             this.ready = Promise.resolve();
-
             /* Create the QR code table if it hasn't been created */
         } else {
             const query = `
         CREATE TABLE ${this.cartSettingsTableName} (
           id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
           shopDomain VARCHAR(511) NOT NULL,
-          title VARCHAR(511) NOT NULL,
-          productId VARCHAR(255) NOT NULL,
-          variantId VARCHAR(255) NOT NULL,
-          handle VARCHAR(255) NOT NULL,
-          discountId VARCHAR(255) NOT NULL,
-          discountCode VARCHAR(255) NOT NULL,
-          destination VARCHAR(255) NOT NULL,
-          scans INTEGER,
+          minimumRequiremenType VARCHAR(255) NOT NULL,
+          discountType VARCHAR(255) NOT NULL,
+          discounts VARCHAR(255) NOT NULL,
           createdAt DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime'))
         )
       `;
@@ -200,78 +152,4 @@ export const GrowCartBD = {
             });
         });
     },
-
-    __addImageUrl: function (qrcode) {
-        try {
-            qrcode.imageUrl = this.__generateQrcodeImageUrl(qrcode);
-        } catch (err) {
-            console.error(err);
-        }
-
-        return qrcode;
-    },
-
-    __generateQrcodeImageUrl: function (qrcode) {
-        return `${Shopify.Context.HOST_SCHEME}://${Shopify.Context.HOST_NAME}/qrcodes/${qrcode.id}/image`;
-    },
-
-    __increaseScanCount: async function (qrcode) {
-        const query = `
-      UPDATE ${this.cartSettingsTableName}
-      SET scans = scans + 1
-      WHERE id = ?
-    `;
-        await this.__query(query, [qrcode.id]);
-    },
-
-    __goToProductView: function (url, qrcode) {
-        return productViewURL({
-            discountCode: qrcode.discountCode,
-            host: url.toString(),
-            productHandle: qrcode.handle,
-        });
-    },
-
-    __goToProductCheckout: function (url, qrcode) {
-        return productCheckoutURL({
-            discountCode: qrcode.discountCode,
-            host: url.toString(),
-            variantId: qrcode.variantId,
-            quantity: DEFAULT_PURCHASE_QUANTITY,
-        });
-    },
 };
-
-/* Generate the URL to a product page */
-function productViewURL({ host, productHandle, discountCode }) {
-    const url = new URL(host);
-    const productPath = `/products/${productHandle}`;
-
-    /* If this QR Code has a discount code, then add it to the URL */
-    if (discountCode) {
-        url.pathname = `/discount/${discountCode}`;
-        url.searchParams.append("redirect", productPath);
-    } else {
-        url.pathname = productPath;
-    }
-
-    return url.toString();
-}
-
-/* Generate the URL to checkout with the product in the cart */
-function productCheckoutURL({ host, variantId, quantity = 1, discountCode }) {
-    const url = new URL(host);
-    const id = variantId.replace(
-        /gid:\/\/shopify\/ProductVariant\/([0-9]+)/,
-        "$1"
-    );
-
-    /* The cart URL resolves to a checkout URL */
-    url.pathname = `/cart/${id}:${quantity}`;
-
-    if (discountCode) {
-        url.searchParams.append("discount", discountCode);
-    }
-
-    return url.toString();
-}
