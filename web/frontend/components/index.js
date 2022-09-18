64ffb5dffcc0ca58ba12d8c69142eae362610c6f
Mithun Biswas <bhoot.biswas@gmail.com>
2022-09-18 10:53:22 +0600

    Update components

--- web/frontend/components/index.js
+++ web/frontend/components/index.js
@@ -1,4 +1,3 @@
 export { ProductsCard } from "./ProductsCard";
-export { RewardsIndex } from "./RewardsIndex";
 export { RewardForm } from "./RewardForm";
 export * from "./providers";
--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,24 +1,13 @@
-import { useNavigate, TitleBar, Loading } from "@shopify/app-bridge-react";
+import { TitleBar, Loading } from "@shopify/app-bridge-react";
 import {
   Card,
-  EmptyState,
   Layout,
   Page,
   SkeletonBodyText,
 } from "@shopify/polaris";
-import { RewardsIndex } from "../components";
+import { RewardForm } from "../components";
 
 export default function HomePage() {
-  /*
-    Add an App Bridge useNavigate hook to set up the navigate function.
-    This function modifies the top-level browser URL so that you can
-    navigate within the embedded app and keep the browser in sync on reload.
-  */
-  const navigate = useNavigate();
-
-  /*
-    These are mock values. Setting these values lets you preview the loading markup and the empty state.
-  */
   const isLoading = false;
   const isRefetching = false;
   const rewards = [
@@ -34,57 +23,18 @@ export default function HomePage() {
     },
   ];
 
-
-  /* Set the QR codes to use in the list */
-  const rewardsMarkup = rewards?.length ? (
-    <RewardsIndex rewards={rewards} loading={isRefetching} />
-  ) : null;
-
-  /* loadingMarkup uses the loading component from AppBridge and components from Polaris  */
-  const loadingMarkup = isLoading ? (
-    <Card sectioned>
-      <Loading />
-      <SkeletonBodyText />
-    </Card>
-  ) : null;
-
-  /* Use Polaris Card and EmptyState components to define the contents of the empty state */
-  const emptyStateMarkup =
-    !isLoading && !rewards?.length ? (
-      <Card sectioned>
-        <EmptyState
-          heading="Create unique reward rules for your shop!"
-          action={{
-            content: "Create reward",
-            onAction: () => navigate("/rewards/new"),
-          }}
-          image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-        >
-          <p>
-            Nudge customers to spend more with your brand.
-          </p>
-        </EmptyState>
-      </Card>
-    ) : null;
-
-  /*
-    Use Polaris Page and TitleBar components to create the page layout,
-    and include the empty state contents set above.
-  */
   return (
     <Page>
-      <TitleBar
-        title="Rewards"
-        primaryAction={{
-          content: "Create reward",
-          onAction: () => navigate("/rewards/new"),
-        }}
-      />
+      <TitleBar title="GrowCart Settings" primaryAction={null} />
       <Layout>
         <Layout.Section>
-          {loadingMarkup}
-          {rewardsMarkup}
-          {emptyStateMarkup}
+          {isLoading ? (
+            <Card sectioned>
+              <Loading />
+              <SkeletonBodyText />
+            </Card>
+          ) : null}
+          <RewardForm />
         </Layout.Section>
       </Layout>
     </Page>
