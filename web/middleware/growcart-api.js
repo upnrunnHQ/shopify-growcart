79c05a6b70cf5f2fc13012828eeed00705b29b2a
Mithun Biswas <bhoot.biswas@gmail.com>
2022-09-18 16:28:29 +0600

    Add api

---
+++ web/middleware/growcart-api.js
@@ -0,0 +1,70 @@
+import { GrowCartDB } from "../qr-codes-db.js";
+import {
+    getSettingsOr404,
+    getShopUrlFromSession,
+    parseSettingsBody,
+    formatSettingsResponse,
+} from "../helpers/growcart.js";
+
+export default function applyGrowCartApiEndpoints(app) {
+    app.post("/api/settings", async (req, res) => {
+        try {
+            const id = await GrowCartDB.create({
+                ...(await parseSettingsBody(req)),
+                shopDomain: await getShopUrlFromSession(req, res),
+            });
+            const response = await GrowCartDB.read(id);
+            res.status(201).send(response);
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    });
+
+    app.patch("/api/settings/:id", async (req, res) => {
+        const settings = await getSettingsOr404(req, res);
+
+        if (settings) {
+            try {
+                await GrowCartDB.update(req.params.id, await parseSettingsBody(req));
+                const response = await formatSettingsResponse(req, res, [
+                    await GrowCartDB.read(req.params.id),
+                ]);
+                res.status(200).send(response[0]);
+            } catch (error) {
+                res.status(500).send(error.message);
+            }
+        }
+    });
+
+    app.get("/api/settings", async (req, res) => {
+        try {
+            const rawCodeData = await GrowCartDB.list(
+                await getShopUrlFromSession(req, res)
+            );
+
+            const response = await formatSettingsResponse(req, res, rawCodeData);
+            res.status(200).send(response);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error.message);
+        }
+    });
+
+    app.get("/api/settings/:id", async (req, res) => {
+        const settings = await getSettingsOr404(req, res);
+
+        if (settings) {
+            const formattedSettings = await formatSettingsResponse(req, res, [settings]);
+            res.status(200).send(formattedSettings[0]);
+        }
+    });
+
+    app.delete("/api/settings/:id", async (req, res) => {
+        const settings = await getSettingsOr404(req, res);
+
+        if (settings) {
+            await GrowCartDB.delete(req.params.id);
+            res.status(200).send();
+        }
+    });
+}
