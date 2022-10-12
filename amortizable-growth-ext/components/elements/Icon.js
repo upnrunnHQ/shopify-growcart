0e791c10f103bebb58a51c5247961b0a4ad4967a
Mithun Biswas <bhoot.biswas@gmail.com>
2022-10-12 11:25:52 +0600

    Fix cart icon

--- amortizable-growth-ext/components/elements/Icon.js
+++ amortizable-growth-ext/components/elements/Icon.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import clsx from 'clsx';
 
-function Icon({ children, className, fill = 'currentColor', stroke, ...props }) {
+function Icon({ children, className, viewBox = '0 0 20 20', fill = 'currentColor', stroke, ...props }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
+            viewBox={viewBox}
             {...props}
             fill={fill}
             stroke={stroke}
@@ -55,16 +55,16 @@ export function IconLock(props) {
     return (
         <Icon {...props} fill="transparent" stroke={props.stroke || 'currentColor'}>
             <title>Lock</title>
-            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
+            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
         </Icon>
     );
 }
 
 export function IconCart(props) {
     return (
-        <Icon {...props} fill="transparent" stroke={props.stroke || 'currentColor'}>
+        <Icon {...props} viewBox="0 0 16 16" fill="transparent" stroke={props.stroke || 'currentColor'}>
             <title>Cart</title>
-            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
         </Icon>
     );
 }
