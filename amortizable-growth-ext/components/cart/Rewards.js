45a62c37138350309f177a430c6c278072ea9aa0
Mithun Biswas <bhoot.biswas@gmail.com>
2022-10-12 11:42:58 +0600

    Remove scroll

--- amortizable-growth-ext/components/cart/Rewards.js
+++ amortizable-growth-ext/components/cart/Rewards.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useDiscounts, useCart } from 'hooks';
+import React from 'react';
+import { useDiscounts } from 'hooks';
 import { IconStar, IconLock } from 'components';
 
 export function Rewards() {
@@ -10,42 +10,10 @@ export function Rewards() {
         rewardsProgress,
         hint
     } = useDiscounts();
-    const {
-        cart
-    } = useCart();
 
     if (discounts.length === 0) {
         return null;
     }
-
-    useEffect(() => {
-        const slider = document.querySelector('.Rewards__list ul');
-        let isDown = false;
-        let startX;
-        let scrollLeft;
-
-        slider.addEventListener('mousedown', (e) => {
-            isDown = true;
-            slider.classList.add('active');
-            startX = e.pageX - slider.offsetLeft;
-            scrollLeft = slider.scrollLeft;
-        });
-        slider.addEventListener('mouseleave', () => {
-            isDown = false;
-            slider.classList.remove('active');
-        });
-        slider.addEventListener('mouseup', () => {
-            isDown = false;
-            slider.classList.remove('active');
-        });
-        slider.addEventListener('mousemove', (e) => {
-            if (!isDown) return;
-            e.preventDefault();
-            const x = e.pageX - slider.offsetLeft;
-            const walk = (x - startX) * 3; //scroll-fast
-            slider.scrollLeft = scrollLeft - walk;
-        });
-    }, [cart]);
 
     return (
         <div className="Rewards CartDetails__Rewards">
